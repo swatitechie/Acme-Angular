e513e0a16ce669f39f15459a2c7145c331ea87a0
swati.samantray <swati.samantray@accenture.com>
2018-08-13 18:21:47 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//directive of html ;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Acme Product Mangement ';
 }
